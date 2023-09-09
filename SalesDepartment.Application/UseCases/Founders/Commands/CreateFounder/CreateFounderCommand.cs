48159ef8eee0ab7f8be853712ef980a43164dd88
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-09-09 19:02:52 +0500

    save

--- SalesDepartment.Application/UseCases/Founders/Commands/CreateFounder/CreateFounderCommand.cs
+++ SalesDepartment.Application/UseCases/Founders/Commands/CreateFounder/CreateFounderCommand.cs
@@ -3,10 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+using SalesDepartment.Application.Common.Interfaces;
+using SalesDepartment.Application.UseCases.Founders.Commands.CreateFounder;
+using SalesDepartment.Domain.Entities;
 
 namespace SalesDepartment.Application.UseCases.Founders.Commands.CreateFounder
 {
-    internal class CreateFounderCommand
+    public class CreateFounderCommand:IRequest<int>
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+
+    public class CreateFounderCommandHandler : IRequestHandler<CreateFounderCommand, int>
+    {
+        private readonly IMapper _mapper;
+        private readonly IApplicationDbContext _context;
+
+        public CreateFounderCommandHandler(IMapper mapper, IApplicationDbContext context)
+        {
+            _mapper = mapper;
+            _context = context;
+        }
+
+        public async Task<int> Handle(CreateFounderCommand request, CancellationToken cancellationToken)
+        {
+            Founder Founder = _mapper.Map<Founder>(request);
+            await _context.Founders.AddAsync(Founder, cancellationToken);
+            await _context.SaveChangesAsync();
+
+            return Founder.Id;
+        }
     }
 }
--- SalesDepartment.Application/UseCases/Founders/Commands/DeleteFounder/DeleteFounderCommand.cs
+++ SalesDepartment.Application/UseCases/Founders/Commands/DeleteFounder/DeleteFounderCommand.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MediatR;
+using SalesDepartment.Application.Common.Exceptions;
+using SalesDepartment.Application.Common.Interfaces;
+using SalesDepartment.Domain.Entities;
 
 namespace SalesDepartment.Application.UseCases.Founders.Commands.DeleteFounder
 {
-    internal class DeleteFounderCommand
+    public record DeleteFounderCommand(int Id) : IRequest;
+    public class DeleteFounderCommandHandler : IRequestHandler<DeleteFounderCommand>
     {
+        private readonly IApplicationDbContext _context;
+
+        public DeleteFounderCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Handle(DeleteFounderCommand request, CancellationToken cancellationToken)
+        {
+            Founder? Founder = await _context.Founders.FindAsync(request.Id, cancellationToken);
+
+            if (Founder is null)
+                throw new NotFoundException(nameof(Founder), request.Id);
+
+            _context.Founders.Remove(Founder);
+
+            await _context.SaveChangesAsync(cancellationToken);
+        }
     }
 }
