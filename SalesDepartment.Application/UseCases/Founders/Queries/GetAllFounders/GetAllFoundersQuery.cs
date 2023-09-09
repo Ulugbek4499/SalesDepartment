f1d5f816ee39dff5229a64c835b7e6d78fdff69c
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-09-09 19:12:51 +0500

    save

--- SalesDepartment.Application/UseCases/Founders/Queries/GetAllFounders/GetAllFoundersQuery.cs
+++ SalesDepartment.Application/UseCases/Founders/Queries/GetAllFounders/GetAllFoundersQuery.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+using SalesDepartment.Application.Common.Interfaces;
+using SalesDepartment.Application.UseCases.Founders.Response;
+using SalesDepartment.Domain.Entities;
 
 namespace SalesDepartment.Application.UseCases.Founders.Queries.GetAllFounders
 {
-    internal class GetAllFoundersQuery
+    public record GetAllFoundersQuery : IRequest<IEnumerable<FounderResponse>>;
+
+    public class GetAllFoundersQueryHandler : IRequestHandler<GetAllFoundersQuery, IEnumerable<FounderResponse>>
     {
+        private readonly IMapper _mapper;
+        private readonly IApplicationDbContext _context;
+
+        public GetAllFoundersQueryHandler(IMapper mapper, IApplicationDbContext context)
+        {
+            _mapper = mapper;
+            _context = context;
+        }
+
+        public Task<IEnumerable<FounderResponse>> Handle(GetAllFoundersQuery request, CancellationToken cancellationToken)
+        {
+            IEnumerable<Founder> Founders = _context.Founders;
+
+            return Task.FromResult(_mapper.Map<IEnumerable<FounderResponse>>(Founders));
+        }
     }
 }
--- SalesDepartment.Application/UseCases/Founders/Response/FounderResponse.cs
+++ SalesDepartment.Application/UseCases/Founders/Response/FounderResponse.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SalesDepartment.Application.UseCases.Founders.Response
 {
-    internal class FounderReponse
+    public class FounderResponse
     {
     }
 }
