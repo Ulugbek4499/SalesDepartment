fbfbd5e169f355e5b9841ab7594e0789320e30ab
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-09-09 18:16:28 +0500

    save

--- SalesDepartment.Application/UseCases/Contracts/Response/ContractResponse.cs
+++ SalesDepartment.Application/UseCases/Contracts/Response/ContractResponse.cs
@@ -8,5 +8,15 @@ namespace SalesDepartment.Application.UseCases.Contracts.Response
 {
     public class ContractResponse
     {
+        public int Id { get; set; }
+        public string ContractNumber { get; set; }
+        public DateTime ContractStartDate { get; set; }
+        public double TotalAmountOfContract { get; set; }
+        public DateTime ContractEndDate { get; set; }
+        public int HomeId { get; set; }
+        public int CustomerId { get; set; }
+        public int FounderId { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifyDate { get; set; }
     }
 }
---
+++ SalesDepartment.Application/UseCases/Customers/Commands/UpdateCustomer/UpdateCustomerCommand.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+using SalesDepartment.Application.Common.Exceptions;
+using SalesDepartment.Application.Common.Interfaces;
+
+namespace SalesDepartment.Application.UseCases.Customers.Commands.UpdateCustomer
+{
+    public class UpdateCustomerCommand:IRequest
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public string Passport { get; set; }
+        public string PassportIssuedBy { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumberOne { get; set; }
+        public string PhoneNumberTwo { get; set; }
+    }
+
+    public class UpdateCustomerCommandHandler : IRequestHandler<UpdateCustomerCommand>
+    {
+        private readonly IMapper _mapper;
+        private readonly IApplicationDbContext _context;
+
+        public UpdateCustomerCommandHandler(IMapper mapper, IApplicationDbContext context)
+        {
+            _mapper = mapper;
+            _context = context;
+        }
+
+        public async Task Handle(UpdateCustomerCommand request, CancellationToken cancellationToken)
+        {
+            Customer? customer = await _context.Customers.FindAsync(request.Id);
+            _mapper.Map(customer, request);
+
+            if (customer is null)
+                throw new NotFoundException(nameof(customer), request.Id);
+        }
+    }
+}
