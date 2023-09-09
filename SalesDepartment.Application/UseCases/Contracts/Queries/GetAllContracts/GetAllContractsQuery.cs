4e968fce10e6ba40d13765f88012d33c64cad70f
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-09-09 18:00:43 +0500

    save

--- SalesDepartment.Application/UseCases/Contracts/Queries/GetAllContracts/GetAllContractsQuery.cs
+++ SalesDepartment.Application/UseCases/Contracts/Queries/GetAllContracts/GetAllContractsQuery.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+using SalesDepartment.Application.Common.Interfaces;
+using SalesDepartment.Application.UseCases.Contracts.Response;
+using SalesDepartment.Domain.Entities;
 
 namespace SalesDepartment.Application.UseCases.Contracts.Queries.GetAllContracts
 {
-    internal class GetAllContractsQuery
+    public record GetAllContractsQuery : IRequest<IEnumerable<ContractResponse>>;
+
+    public class GetAllContractsQueryHandler : IRequestHandler<GetAllContractsQuery, IEnumerable<ContractResponse>>
     {
+        private readonly IMapper _mapper;
+        private readonly IApplicationDbContext _context;
+
+        public GetAllContractsQueryHandler(IMapper mapper, IApplicationDbContext context)
+        {
+            _mapper = mapper;
+            _context = context;
+        }
+
+        public Task<IEnumerable<ContractResponse>> Handle(GetAllContractsQuery request, CancellationToken cancellationToken)
+        {
+            IEnumerable<Contract> Contracts = _context.Contracts;
+
+            return Task.FromResult(_mapper.Map<IEnumerable<ContractResponse>>(Contracts));
+        }
     }
 }
--- SalesDepartment.Application/UseCases/Contracts/Queries/GetContractById/GetContractByIdQuery.cs
+++ SalesDepartment.Application/UseCases/Contracts/Queries/GetContractById/GetContractByIdQuery.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AutoMapper;
+using MediatR;
+using SalesDepartment.Application.Common.Exceptions;
+using SalesDepartment.Application.Common.Interfaces;
+using SalesDepartment.Application.UseCases.Contracts.Response;
+using SalesDepartment.Domain.Entities;
 
 namespace SalesDepartment.Application.UseCases.Contracts.Queries.GetContractById
 {
-    internal class GetContractByIdQuery
+    public record GetContractByIdQuery(int Id) : IRequest<ContractResponse>;
+
+    public class GetContractByIdQueryHandler : IRequestHandler<GetContractByIdQuery, ContractResponse>
     {
+        IApplicationDbContext _dbContext;
+        IMapper _mapper;
+
+        public GetContractByIdQueryHandler(IApplicationDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public async Task<ContractResponse> Handle(GetContractByIdQuery request, CancellationToken cancellationToken)
+        {
+            var Contract = FilterIfContractExsists(request.Id);
+
+            var result = _mapper.Map<ContractResponse>(Contract);
+            return await Task.FromResult(result);
+        }
+
+        private Contract FilterIfContractExsists(int id)
+            => _dbContext.Contracts
+                .Find(id)
+                     ?? throw new NotFoundException(
+                            " There is no Contract with this Id. ");
     }
 }
