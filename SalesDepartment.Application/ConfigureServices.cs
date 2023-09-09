63d636f95e60dd552beaa6ee829498f1d94a1477
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-09-09 19:44:51 +0500

    save

--- SalesDepartment.Application/ConfigureServices.cs
+++ SalesDepartment.Application/ConfigureServices.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Reflection;
+using MediatR;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using SalesDepartment.Application.Common.Behaviours;
 
 namespace SalesDepartment.Application
 {
-    internal class ConfigureServices
+    public static class ConfigureServices
     {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(option =>
+            {
+                option.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+                option.AddBehavior(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
+                option.AddBehavior(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+
+            });
+
+            return services;
+        }
     }
 }
