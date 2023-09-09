21510fa39d523984c1d27d7b493ba4160191fd4d
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-09-09 18:20:13 +0500

    save

---
+++ SalesDepartment.Application/UseCases/Customers/Queries/GetAllCustomers/GetAllCustomersQuery.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+using SalesDepartment.Application.Common.Interfaces;
+using SalesDepartment.Application.UseCases.Customers.Response;
+using SalesDepartment.Domain.Entities;
+
+namespace SalesDepartment.Application.UseCases.Customers.Queries.GetAllCustomers
+{
+    public record GetAllCustomersQuery : IRequest<IEnumerable<CustomerResponse>>;
+
+    public class GetAllCustomersQueryHandler : IRequestHandler<GetAllCustomersQuery, IEnumerable<CustomerResponse>>
+    {
+        private readonly IMapper _mapper;
+        private readonly IApplicationDbContext _context;
+
+        public GetAllCustomersQueryHandler(IMapper mapper, IApplicationDbContext context)
+        {
+            _mapper = mapper;
+            _context = context;
+        }
+
+        public Task<IEnumerable<CustomerResponse>> Handle(GetAllCustomersQuery request, CancellationToken cancellationToken)
+        {
+            IEnumerable<Customer> Customers = _context.Customers;
+
+            return Task.FromResult(_mapper.Map<IEnumerable<CustomerResponse>>(Customers));
+        }
+    }
+}
