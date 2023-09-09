5121dcbab646d4a49db20a58434d4f61eee03072
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-09-09 21:26:40 +0500

    save

--- SalesDepartment.Application/UseCases/Payments/Response/PaymentResponse.cs
+++ SalesDepartment.Application/UseCases/Payments/Response/PaymentResponse.cs
@@ -2,5 +2,14 @@
 {
     public class PaymentResponse
     {
+        public int Id { get; set; }
+        public string PaymentNumber { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public double Amount { get; set; }
+        public string AmountsInWords { get; set; }
+        public int ContractId { get; set; }
+        public int PaymentTypeId { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifyDate { get; set; }
     }
 }
