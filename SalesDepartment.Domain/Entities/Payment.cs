de538ec415e71f9cd028f7340900caa690eecb67
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-09-09 12:09:04 +0500

    save

--- SalesDepartment.Domain/Entities/Payment.cs
+++ SalesDepartment.Domain/Entities/Payment.cs
@@ -11,11 +11,14 @@ namespace SalesDepartment.Domain.Entities
     {
         public string PaymentNumber { get; set; }
         public DateTime PaymentDate { get; set; }
+
         public string PassportNumber { get; set; }
         public string ContractNumber { get; set; }
         public string FullName { get; set; }
+
         public double Amount { get; set; }
         public string AmountsInWords { get; set; }
+        
         public int PaymentId { get; set; }
         public PaymentType PaymentType { get; set; }
     }
