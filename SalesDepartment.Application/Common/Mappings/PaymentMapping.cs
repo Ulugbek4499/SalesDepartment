aa96fec297a72b17b2c1f808059f2308c0704683
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-09-10 12:43:55 +0500

    save

---
+++ SalesDepartment.Application/Common/Mappings/PaymentMapping.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using SalesDepartment.Application.UseCases.Payments.Commands.CreatePayment;
+using SalesDepartment.Application.UseCases.Payments.Commands.DeletePayment;
+using SalesDepartment.Application.UseCases.Payments.Commands.UpdatePayment;
+using SalesDepartment.Application.UseCases.Payments.Response;
+using SalesDepartment.Domain.Entities;
+
+namespace SalesDepartment.Application.Common.Mappings
+{
+    public class PaymentMapping : Profile
+    {
+        public PaymentMapping()
+        {
+            CreateMap<CreatePaymentCommand, Payment>().ReverseMap();
+            CreateMap<DeletePaymentCommand, Payment>().ReverseMap();
+            CreateMap<UpdatePaymentCommand, Payment>().ReverseMap();
+            CreateMap<PaymentResponse, Payment>().ReverseMap();
+        }
+    }
+}
---
+++ SalesDepartment.Application/Common/Mappings/PaymentTypeMapping.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using SalesDepartment.Application.UseCases.PaymentTypes.Commands.CreatePaymentType;
+using SalesDepartment.Application.UseCases.PaymentTypes.Commands.DeletePaymentType;
+using SalesDepartment.Application.UseCases.PaymentTypes.Commands.UpdatePaymentType;
+using SalesDepartment.Application.UseCases.PaymentTypes.Response;
+using SalesDepartment.Domain.Entities;
+
+namespace SalesDepartment.Application.Common.Mappings
+{
+    public class PaymentTypeMapping : Profile
+    {
+        public PaymentTypeMapping()
+        {
+            CreateMap<CreatePaymentTypeCommand, PaymentType>().ReverseMap();
+            CreateMap<DeletePaymentTypeCommand, PaymentType>().ReverseMap();
+            CreateMap<UpdatePaymentTypeCommand, PaymentType>().ReverseMap();
+            CreateMap<PaymentTypeResponse, PaymentType>().ReverseMap();
+        }
+    }
+}
