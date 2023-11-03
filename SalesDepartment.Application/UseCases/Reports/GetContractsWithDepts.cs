1509f18df12318295df699684e67a0d6aeb43c9d
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-11-03 15:34:59 +0500

    save

--- SalesDepartment.Application/UseCases/Reports/GetContractsWithDepts.cs
+++ SalesDepartment.Application/UseCases/Reports/GetContractsWithDepts.cs
@@ -30,41 +30,42 @@ namespace SalesDepartment.Application.UseCases.Reports
                 var contract = _context.Contracts.Find(entity.Id);
 
                 Dictionary<DateTime, decimal> actualPaymentSchedule = contract.Payments
-                             .ToDictionary(payment => payment.PaymentDate, payment => payment.Amount);
+                    .ToDictionary(payment => payment.PaymentDate, payment => payment.Amount);
 
-
-                Dictionary<DateTime, decimal> paymentSchedule = new Dictionary<DateTime, decimal>();
+                Dictionary<string, decimal> paymentSchedule = new Dictionary<string, decimal>();
 
                 DateTime paymentDate = contract.PaymentStartDate;
+                int paymentIndex = 1;
 
                 for (int i = 1; i <= contract.NumberOfMonths; i++)
                 {
-                    decimal paymentAmount = contract.InAdvancePaymentOfContract +
-                        (i * contract.AmountOfMonthlyPayment);
+                    decimal paymentAmount = contract.InAdvancePaymentOfContract + (i * contract.AmountOfMonthlyPayment);
 
-                    paymentSchedule.Add(paymentDate, paymentAmount);
+                    // Generate a unique key based on the paymentIndex
+                    string paymentKey = $"Payment-{paymentIndex}";
 
-                    paymentDate = paymentDate.AddMonths(1);
+                    paymentSchedule.Add(paymentKey, paymentAmount);
+
+                    paymentIndex++;
                 }
 
                 DateTime calculationDate = DateTime.Now;
                 var sumOfPayments = contract.Payments.Sum(x => x.Amount);
 
-                decimal sumForCurrentMonth=0;
+                decimal sumForCurrentMonth = 0;
 
                 foreach (var entry in paymentSchedule)
                 {
-                    if (entry.Key.Month <= calculationDate.Month
-                        && entry.Key.Year <= calculationDate.Year)
+                    if (entry.Key.StartsWith("Payment-") && int.TryParse(entry.Key.Substring("Payment-".Length), out int index))
                     {
-                        sumForCurrentMonth = entry.Value;
+                        if (index <= calculationDate.Month)
+                        {
+                            sumForCurrentMonth = entry.Value;
+                        }
                     }
                 }
 
-                decimal deptAmount = 0;
-
-                deptAmount = sumForCurrentMonth - sumOfPayments;
-
+                decimal deptAmount = sumForCurrentMonth - sumOfPayments;
 
                 ContractResponse contractResponse = new ContractResponse()
                 {
@@ -86,7 +87,7 @@ namespace SalesDepartment.Application.UseCases.Reports
                     ModifyDate = contract.ModifyDate,
                     Payments = contract.Payments,
                     DeptAmout = deptAmount,
-                    ScheduledInfo = paymentSchedule,
+                    ScheduledInfo2 = paymentSchedule,
                     ActualInfo = actualPaymentSchedule
                 };
 
@@ -95,5 +96,84 @@ namespace SalesDepartment.Application.UseCases.Reports
 
             return contractResponses.ToArray();
         }
+
+
+        /*
+                public async Task<ContractResponse[]> Handle(GetContractsWithDepts request, CancellationToken cancellationToken)
+                {
+                    var contracts = await _context.Contracts.ToArrayAsync();
+
+                    var contractResponses = new List<ContractResponse>();
+
+                    foreach (var entity in contracts)
+                    {
+                        var contract = _context.Contracts.Find(entity.Id);
+
+                        Dictionary<DateTime, decimal> actualPaymentSchedule = contract.Payments
+                                     .ToDictionary(payment => payment.PaymentDate, payment => payment.Amount);
+
+
+                        Dictionary<DateTime, decimal> paymentSchedule = new Dictionary<DateTime, decimal>();
+
+                        DateTime paymentDate = contract.PaymentStartDate;
+
+                        for (int i = 1; i <= contract.NumberOfMonths; i++)
+                        {
+                            decimal paymentAmount = contract.InAdvancePaymentOfContract +
+                                (i * contract.AmountOfMonthlyPayment);
+
+                            paymentSchedule.Add(paymentDate, paymentAmount);
+
+                            paymentDate = paymentDate.AddMonths(1);
+                        }
+
+                        DateTime calculationDate = DateTime.Now;
+                        var sumOfPayments = contract.Payments.Sum(x => x.Amount);
+
+                        decimal sumForCurrentMonth=0;
+
+                        foreach (var entry in paymentSchedule)
+                        {
+                            if (entry.Key.Month <= calculationDate.Month
+                                && entry.Key.Year <= calculationDate.Year)
+                            {
+                                sumForCurrentMonth = entry.Value;
+                            }
+                        }
+
+                        decimal deptAmount = 0;
+
+                        deptAmount = sumForCurrentMonth - sumOfPayments;
+
+
+                        ContractResponse contractResponse = new ContractResponse()
+                        {
+                            Id = entity.Id,
+                            ContractNumber = contract.ContractNumber,
+                            ContractStartDate = contract.ContractStartDate,
+                            PaymentStartDate = contract.PaymentStartDate,
+                            TotalAmountOfContract = contract.TotalAmountOfContract,
+                            InAdvancePaymentOfContract = contract.InAdvancePaymentOfContract,
+                            NumberOfMonths = contract.NumberOfMonths,
+                            AmountOfMonthlyPayment = contract.AmountOfMonthlyPayment,
+                            HomeId = contract.HomeId,
+                            Home = contract.Home,
+                            CustomerId = contract.CustomerId,
+                            Customer = contract.Customer,
+                            FounderId = contract.FounderId,
+                            Founder = contract.Founder,
+                            CreatedDate = contract.CreatedDate,
+                            ModifyDate = contract.ModifyDate,
+                            Payments = contract.Payments,
+                            DeptAmout = deptAmount,
+                            ScheduledInfo = paymentSchedule,
+                            ActualInfo = actualPaymentSchedule
+                        };
+
+                        contractResponses.Add(contractResponse);
+                    }
+
+                    return contractResponses.ToArray();
+                }*/
     }
 }
